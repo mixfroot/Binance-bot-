016e05c4a501d7e8f3b924f4f05f1fbf9af90366
mixfroot <r6champ17@gmail.com>
2026-09-10 23:10:33 +0300

    Update btc_volume_bot.py

--- btc_volume_bot.py
+++ btc_volume_bot.py
@@ -25,7 +25,7 @@ CHAT_ID   = "6263967739"
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
 # Volume Z-Score settings (exactly matching the Pine Script)
-TIMEFRAME          = "5m"          # change to "1m", "15m", etc. if you want
+TIMEFRAME          = "1m"          # change to "1m", "15m", etc. if you want
 Z_LENGTH           = 1000
 ALERT_Z_MIN        = 1.5           # green and above
 
